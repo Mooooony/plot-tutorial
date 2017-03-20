b60b484daf33cd4037f4b0fcdf4b6e47c7747a06
wanglong <2402997445@qq.com>
2017-03-20 10:57:18 +0800

    line

---
+++ some numbers.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4])
+plt.ylabel('some numbers')
+plt.show()
+'''You may be wondering why the
+x-axis ranges from 0-3 and the y-axis from 1-4. If you provide a single list or array to the plot()
+ command, matplotlib assumes it is a sequence of y values, and automatically generates the x values for you.
+ Since python ranges start with 0, the default x vector has the same length as y but starts with 0. Hence the x
+  data are [0,1,2,3].'''
+
+
